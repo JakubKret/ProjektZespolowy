09592d9a5a9b5f75e13f256ed01c9792c6084e32
JakubKret <jakub.kret04@gmail.com>
2026-05-24 01:43:13 +0200

    Final polishes to the first prototype

--- hdk-system/mobile-app/android/app/src/main/kotlin/com/example/hdk_mobile_app/MainActivity.kt
+++ hdk-system/mobile-app/android/app/src/main/kotlin/com/example/hdk_mobile_app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.hdk_mobile_app
+package com.example.krwiodawstwo
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() { 
+}
